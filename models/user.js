70638be4338d30cf006a03fd8b16640fa270b9fa
Rui Huang <huang87924@gmail.com>
2020-08-18 15:27:56 -0400

    modified:   models/user.js 	new file:   public/images/icon.png 	modified:   public/libs/bootstrap/css/bootstrap.css 	modified:   views/topic/edit.html

--- models/user.js
+++ models/user.js
@@ -47,20 +47,22 @@ var UserSchema = new Schema({
 
 UserSchema.plugin(BaseModel);
 UserSchema.virtual('avatar_url').get(function () {
-  var url = this.avatar || ('https://gravatar.com/avatar/' + utility.md5(this.email.toLowerCase()) + '?size=48');
+  // var url = this.avatar || ('https://gravatar.com/avatar/' + utility.md5(this.email.toLowerCase()) + '?size=48');
 
-  // www.gravatar.com 被墙
-  url = url.replace('www.gravatar.com', 'gravatar.com');
+  // // www.gravatar.com 被墙
+  // url = url.replace('www.gravatar.com', 'gravatar.com');
 
-  // 让协议自适应 protocol，使用 `//` 开头
-  if (url.indexOf('http:') === 0) {
-    url = url.slice(5);
-  }
+  // // 让协议自适应 protocol，使用 `//` 开头
+  // if (url.indexOf('http:') === 0) {
+  //   url = url.slice(5);
+  // }
 
-  // 如果是 github 的头像，则限制大小
-  if (url.indexOf('githubusercontent') !== -1) {
-    url += '&s=120';
-  }
+  // // 如果是 github 的头像，则限制大小
+  // if (url.indexOf('githubusercontent') !== -1) {
+  //   url += '&s=120';
+  // }
+
+  var url = "/public/images/icon.png"
 
   return url;
 });
